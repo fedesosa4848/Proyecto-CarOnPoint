84194ec35cb9d4832d1d333ef5169d50ed77bd44
Federico Sosa <federico.sosa4848@gmail.com>
2024-05-28 21:02:18 -0300

    nuevos templates

--- inicio/views.py
+++ inicio/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import Template,Context,loader
 from datetime import datetime
-
+import random
 # Create your views here.
 #vista
 def inicio(request):
@@ -21,5 +21,54 @@ def template2(request,nombre,apellido):
     
     template =  Template(archivo_abierto.read())
     
+    archivo_abierto.close()
+    fecha = datetime.now()
     
-    return HttpResponse('')
+    datos = {
+        'fecha' : datetime.now(),
+        'nombre' : nombre,
+        'apellido' : apellido
+    }
+    
+    contexto = Context(datos)
+    template_renderizado = template.render(contexto)
+    
+    
+    return HttpResponse(template_renderizado)
+
+def template3(request,nombre,apellido):
+        
+    template = loader.get_template('template3.html')
+    
+    fecha = datetime.now()
+    
+    datos = {
+        'fecha' : datetime.now(),
+        'nombre' : nombre,
+        'apellido' : apellido
+    }
+    
+    template_renderizado = template.render(datos)
+    
+    return HttpResponse(template_renderizado)
+
+def template4(request,nombre,apellido):
+        
+    
+    fecha = datetime.now()
+    
+    datos = {
+        'fecha' : datetime.now(),
+        'nombre' : nombre,
+        'apellido' : apellido
+    }
+
+    
+    return render(request,'template2.html',datos), 
+
+def probando (request):
+    
+    lista = list(range(500))
+    
+    numeros = random.choices(lista, k = 50)
+    return render(request,'probando_if_for.hmtl',{"numero" : numeros})
